d9ab1c3f16070ca75903a090c2912a4d92a5d486
HandeG <handegombe0@gmail.com>
2024-08-05 17:52:19 +0300

    try 11

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { getDocs, collection , query ,orderBy } from "firebase/firestore";
+import { getDocs, collection, query, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
 import "./Pages.css";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Placeholder } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Footer from '../components/Footer'
+import Footer from "../components/Footer";
+
 function Blog({ isAuth }) {
   const [data, setData] = useState([]);
 
@@ -23,20 +24,27 @@ function Blog({ isAuth }) {
 
   return (
     <div>
-      <Container id="blog-posts-container">
-        {data.map((post) => (
-          <Card id="posts-blog">
-            <Card.Body>
-              <Card.Title as="h5">{post.titleVal}</Card.Title>
-              <Card.Text>{post.TxtVal}</Card.Text>
-              <Link to={`/blog/${post.id}`} >
-                Posta git
-              </Link>
-            </Card.Body>
-          </Card>
-        ))}
-      </Container>
-      <Footer/>
+      {data.length > 0 ? (
+        <Container id="blog-posts-container">
+          {data.map((post) => (
+            <Card id="posts-blog">
+              <Card.Body>
+                <Card.Title as="h5">{post.titleVal}</Card.Title>
+                <Card.Text>{post.TxtVal}</Card.Text>
+                <Link to={`/blog/${post.id}`}>Posta git</Link>
+              </Card.Body>
+            </Card>
+          ))}
+        </Container>
+      ) : (
+        <Container id="blog-posts-container">
+          <Container id="posts-blog">
+          
+          </Container>
+        </Container>
+      )}
+
+      <Footer />
     </div>
   );
 }
